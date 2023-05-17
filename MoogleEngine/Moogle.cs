92ea596a4d08eab6991f1f74af944ba1098f72a5
Daniel Collazo <danielaldana829@gmail.com>
2023-05-16 20:10:52 -0500

    Update Readme.md

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -6,11 +6,11 @@ public static class Moogle
     public static SearchResult Query(string query) {
         // Modifique este método para responder a la búsqueda
 
-        SearchItem[] items = new SearchItem[3] {
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
-        };
+        SearchItem[] items = ElAprobado.Busqueda(query);
+
+        for(int i = 0; i < items.Length; i++){
+            System.Console.WriteLine(items[i].Title);
+        }
 
         return new SearchResult(items, query);
     }
--- MoogleEngine/SearchItem.cs
+++ MoogleEngine/SearchItem.cs
@@ -9,9 +9,23 @@ public class SearchItem
         this.Score = score;
     }
 
+    public SearchItem(string v1, double v2, Dictionary<string, List<string>> frecuencia, string v3, string primerasPalabras)
+    {
+        V1 = v1;
+        V2 = v2;
+        Frecuencia = frecuencia;
+        V3 = v3;
+        PrimerasPalabras = primerasPalabras;
+    }
+
     public string Title { get; private set; }
 
     public string Snippet { get; private set; }
 
     public float Score { get; private set; }
+    public string V1 { get; }
+    public double V2 { get; }
+    public Dictionary<string, List<string>> Frecuencia { get; }
+    public string V3 { get; }
+    public string PrimerasPalabras { get; }
 }
